0d447970653c9874c25499b1107ea1d505f8eb2c
JabaKhizanishvili <xizanishvili.99@gmail.com>
2025-05-01 16:56:38 +0400

    trasinings accordion and visual changes

--- resources/views/front/categories.blade.php
+++ resources/views/front/categories.blade.php
@@ -8,7 +8,6 @@
 
 @section('css')
 
-    /*<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">*/
     <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
 @endsection
 
